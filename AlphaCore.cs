ba0e5b5d0376d6e13328feed9a3f25759983ec72
Иван Карев <I.Karev@globus.ru>
2022-02-24 22:55:02 +0300

    info window

--- AlphaCore.cs
+++ AlphaCore.cs
@@ -546,7 +546,7 @@ namespace Alpha
 				}
 			var dist = _tasks.Count > 0 ? Vector3.Distance(GameController.Player.Pos, _tasks.First().WorldPosition): 0;
 			var targetDist = _lastTargetPosition == null ? "NA" : Vector3.Distance(GameController.Player.Pos, _lastTargetPosition).ToString();
-			Graphics.DrawText($"Follow Enabled: {Settings.IsFollowEnabled.Value}", new Vector2(500, 120));
+			Graphics.DrawText($"Follow Enabled: {Settings.IsFollowEnabled.Value}", new Vector2(900, 420));
 			//added Take Waypoints checker, similar to copilot logic
 			Graphics.DrawText($"Take Waypoints: {Settings.IsToggleWaypointsEnabled.Value}", new Vector2(520, 120));
 			Graphics.DrawText($"Task Count: {_tasks.Count} Next WP Distance: {dist} Target Distance: {targetDist}", new Vector2(500, 140));
